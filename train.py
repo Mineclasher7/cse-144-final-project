3ca8413e9a3c14b7bc70f5bfd9d772835aeb1ec6
mineclasher7 <connorjjuricich@gmail.com>
2026-06-03 18:38:35 -0700

    v2.411

--- train.py
+++ train.py
@@ -103,7 +103,7 @@ def get_embeddings(img_list):
 # -----------------------------
 def main():
     train_dir = "/content/drive/MyDrive/train"
-    EPOCHS = 30
+    EPOCHS = 35
     BATCH_SIZE = 32
 
     dataset = ImageDataset(train_dir)
